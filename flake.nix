49bebc5e516928659d50bec4def0374014bc5104
Modularius <daniel.kirk@rocketmail.com>
2024-08-02 16:45:16 +0000

    Flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    pipeline = "path:../supermusr-data-pipeline";
+    pipeline.url = "github:STFC-ICD-Research-and-Design/supermusr-data-pipeline";
   };
   outputs = {
     self,
@@ -11,11 +11,22 @@
   } : flake-utils.lib.eachDefaultSystem
     ( system:
         let
-          pkgs = (import nixpkgs);
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
         in {
           devShell = pkgs.mkShell {
+            buildInputs = [
+              pkgs.python312
+              pkgs.python312.pip
+              pkgs.python312.pandas
+              pkgs.python312Packages.matplotlib
+              pkgs.python312.numpy
+              pkgs.python312.ipywidgets
+              pkgs.python312Packages.conda
+              ];
             inputsFrom  = [
-              pipeline.devShell.${system}.devShell
+              pipeline.devShell.${system}
             ];
           };
         }
