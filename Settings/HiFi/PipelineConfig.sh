e677a87e1d81aef3fb18bc7e92e1c3fc04512a91
Modularius <daniel.kirk@rocketmail.com>
2024-09-27 10:36:07 +0000

    Updated Hifi settings

--- Settings/HiFi/PipelineConfig.sh
+++ Settings/HiFi/PipelineConfig.sh
@@ -3,15 +3,15 @@ BROKER="130.246.55.29:9092"
 
 # Broker Topics
 TRACE_TOPIC=daq-traces-in
-DAT_EVENT_TOPIC=daq-events-in
-FRAME_EVENT_TOPIC=ics-_events
+DAT_EVENT_TOPIC=daq-events
+FRAME_EVENT_TOPIC=frame-events
 CONTROL_TOPIC=ics-control-change
 LOGS_TOPIC=ics-metadata
 SELOGS_TOPIC=ics-metadata
 ALARMS_TOPIC=ics-alarms
 
 # Digitisers Expected from Broker
-DIGITIZERS=$(build_digitiser_argument 7)
+DIGITIZERS="-d4 -d5 -d6 -d7 -d8 -d9 -d10 -d11"
 
 # Output Path
-NEXUS_OUTPUT_PATH="Output/HiFi"
+NEXUS_OUTPUT_PATH="/mnt/hifi-archive/incoming"
