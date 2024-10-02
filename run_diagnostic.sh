0f744e9348130d690e43280b31e6218dca256887
Modularius <daniel.kirk@rocketmail.com>
2024-10-02 13:24:28 +0000

    Diagnostic

---
+++ run_diagnostic.sh
@@ -0,0 +1,10 @@
+## Setup Pipeline
+. ./Settings/PipelineSetup.sh
+
+
+### Enact Pipeline Configuration for Chosen Broker
+#### Local
+. ./Settings/HiFi/PipelineConfig.sh
+
+# Diagnose Daq Traces
+${APPLICATION_PREFIX}/diagnostics daq-trace --broker $BROKER --topic $TRACE_TOPIC  --group vis-3
