f81267b40f5ac1c5a0ed02fa2e22956b3d0c7929
Modularius <daniel.kirk@rocketmail.com>
2024-12-06 15:55:29 +0000

    Typo

--- Scripts/compose.sh
+++ Scripts/compose.sh
@@ -1,1 +1,1 @@
-podman compose -e Docker/local_broker.env  -e Docker/local_setup.env -e Docker/pipeline.env -f Docker/docker-compose. up
+podman-compose --env-file Docker/local_broker.env --env-file Docker/local_setup.env --env-file Docker/pipeline.env -f Docker/docker-compose.yaml --dry-run up
